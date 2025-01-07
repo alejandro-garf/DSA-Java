c95cd728b5bec87cc6c59862d7dee15dbf9b2272
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-07 12:45:53 -0800

    peek method

--- Stacks/main.java
+++ Stacks/main.java
@@ -10,10 +10,19 @@ public class main{
         stack.push("Falling Down");
         stack.push("Careless Whisper");
 
-        System.out.print(stack);
+        System.out.println(stack);
 
-        stack.pop(); //Don't need to list an object becaise pop will always remove the top most method from the stack
+        //stack.pop(); //Don't need to list an object becaise pop will always remove the top most method from the stack
 
-        System.out.print(stack);
+        // pop returns the value of the object that I popped, so I can then assign it to a variable 
+        String Favsong = stack.pop();
+        System.out.println(Favsong);
+
+        System.out.println(stack);
+
+        // Peek method "peeks" at what object is at the top of the stack without changing anything
+        System.out.println(stack.peek());
+
+
     }
 }
