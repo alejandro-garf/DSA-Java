16f769fd4260dd00f1f257a99c7888c76fa052c1
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-07 12:39:33 -0800

    pop method

--- Stacks/main.java
+++ Stacks/main.java
@@ -4,7 +4,16 @@ public class main{
      
         Stack<String> stack = new Stack<String>();
 
-        System.out.print(stack.empty());
 
+        stack.push("Disaster");
+        stack.push("APT");
+        stack.push("Falling Down");
+        stack.push("Careless Whisper");
+
+        System.out.print(stack);
+
+        stack.pop(); //Don't need to list an object becaise pop will always remove the top most method from the stack
+
+        System.out.print(stack);
     }
 }
