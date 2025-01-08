190948c9b8457e2604ce8e3fbfec21ebb797a7d2
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-08 15:56:39 -0800

    Queue Creation add, remove,and peek

--- Qeues/main.java
+++ Qeues/main.java
@@ -1,11 +1,25 @@
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class main{
 
     public static void main(String[] args){
 
+        Queue<String> queue = new LinkedList<String>();
 
+        queue.offer("First Place");
+        queue.offer("Second Place");
+        queue.offer("Third Place");
+        queue.offer("Fourth Place");
+        queue.offer("Fifth Place");
+
+        System.out.println(queue);
+        System.out.println(queue.peek());
+
+        queue.poll();
+
+        System.out.println(queue);
+        System.out.println(queue.peek());
 
     }
 }
