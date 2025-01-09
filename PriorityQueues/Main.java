156713d9ec3732722c95eb6bdde63dcabf0d88e0
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-08 16:23:53 -0800

    Reverse Order

--- PriorityQueues/Main.java
+++ PriorityQueues/Main.java
@@ -4,7 +4,8 @@ public class Main{
 
     public static void main(String[] args) {
 
-        Queue<Double> queue = new LinkedList<>();
+        Queue<Double> queue = new PriorityQueue <>(Collections.reverseOrder()); //When using Priority Queues with Numbers, they will be ordered in ascending order    
+                                                                                // Collections.reverseOrder() makes them go in descending order
 
         queue.offer(3.0);
         queue.offer(1.5);
