bbc68ae85c6d9d5359bea5880f58ecb9ecec6a84
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-14 16:46:30 -0800

    Linkedlist/stack

--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -1,7 +1,23 @@
+import java.util.LinkedList;
+
 public class Main{
 
     public static void main(String[] args) {
 
+        LinkedList<String> linkedlist = new LinkedList<String>();
+
+        // Treat it like a stack
+        linkedlist.push("A");
+        linkedlist.push("V");
+        linkedlist.push("Q");
+
+        System.out.println(linkedlist);
+
+        linkedlist.pop();
+        System.out.println(linkedlist);
+
+
+        // Treat it like a Qeue
         
 
     }
