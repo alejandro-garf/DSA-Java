ee529d3c81612f5e031195a5bb79e775d00062a8
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-15 19:21:12 -0800

    Overload Constructors

--- DynamicArrays/main.java
+++ DynamicArrays/main.java
@@ -1,11 +1,15 @@
-import java.util.ArrayList;
 
 public class main{
 
     public static void main(String[]args) {
 
-        ArrayList<String> arraylist = new ArrayList<String>();
+        //ArrayList<String> arraylist = new ArrayList<String>();
 
+        DynamicArray dynamicArray = new DynamicArray();
+        System.out.println(dynamicArray.capacity);
+
+        DynamicArray dynamiccArray = new DynamicArray(15);
+        System.out.println(dynamiccArray.capacity);
 
     }
 }
