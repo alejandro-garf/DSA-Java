9c4da12ff773e3d64e4b74ae5dc470c823dacc84
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-15 19:37:35 -0800

    Mehods templates

--- DynamicArrays/DynamicArray.java
+++ DynamicArrays/DynamicArray.java
@@ -11,4 +11,48 @@ public class DynamicArray {
         this.capacity = capacity;
         this.array = new Object[capacity];
     }
+
+    // Methods
+
+    // Adds Object Data
+    public void add(Object data){
+
+    }
+
+    // Inserts Object Data at specefic index 
+    public void insert(int index, Object data){
+
+    }
+
+    // delets object data
+    public void delete(Object data){
+
+    }
+
+    //search
+    public int search(Object data){
+        return -1;
+    }
+
+    // grows array
+    public void grow(){
+
+    }
+
+    // Shrinks array
+    public void shrink(){
+
+    }
+
+    // Returns true if array is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // No idea lol, String?
+    public String toString() {
+        return null;
+    }
+
+
 }
