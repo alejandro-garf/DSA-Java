21a5c14404bd3d578ec177e46669757e19dbe76f
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-28 18:37:49 -0800

    int loop

---
+++ Algo/sum2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    //brute force
+    function sum_of_numbers(n):
+        total = 0
+        for i in range(1, n + 1):
+            total += i
+        return tota;
+
+    // Mathmatical formula 
+    function sum_of_nubmers(n):
+        return n * (n + 1) / 2
+}
