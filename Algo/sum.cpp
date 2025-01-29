aa3e179c79f32c1c28b99a37852dcf06f7f9a0c0
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-28 18:31:52 -0800

    sum algo

---
+++ Algo/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// Algo is improved because the second algo checks to see if the list is empty
+int main(){
+
+    def firstTria_sum(S):
+    total = 0;
+    for x in S:
+        total += x
+    return 0;
+
+    def secondTrial_sum(S):
+        if len(S) == 0
+            return 0;
+    else:
+        total = 0
+        for x in S:
+            total += x
+        return total
+}
