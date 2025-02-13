0aab6b061e55c19eb4cd341bc5b79ca574ae2714
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-12 23:39:23 -0800

    simple java program

---
+++ Algo/Studying/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        int n = 10;
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Hello Gael");
+        }
+    }
+}
